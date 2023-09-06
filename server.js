123ff3db3fcf2d51c43f10c07f8db688da6530ad
Sydney Aurelius <saurelius@ufl.edu>
2023-09-06 14:06:19 -0400

    Added listingData

--- server.js
+++ server.js
@@ -9,21 +9,17 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  if(url.parse(request.url).pathname == '/listings'){
+    // send the data in the listingData variable as a response
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(listingData));
+  }
+  else{
+    response.writeHead(404);
+    response.write('404 Not Found');
+  }
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+  response.end();
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
@@ -38,14 +34,19 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
    */
 
     //Check for errors
-  
+    if (err) throw err;
+    //console.log(data);
 
-   //Save the sate in the listingData variable already defined
-  
+   //Save the state in the listingData variable already defined
+   listingData = JSON.parse(data);
 
   //Creates the server
-  
-  //Start the server
+  var server = http.createServer(requestHandler);
 
+  //Start the server
+  server.listen(port, function() {
+    //once the server is listening, this callback function is executed
+    console.log('Server listening on: http://127.0.0.1:' + port);
+  });
 
 });
